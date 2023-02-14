c3396d8525fcb98673aafd5a74b51203d5407c39
eliabdo <elieabdo360@gmail.com>
2023-02-14 16:36:04 +0200

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
-const UNSPLASH_KEY= process.env.REACT_APP_UNSPLASH_KEY;
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  
-  const [word, setWord ] = useState('')
-
+  const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+  console.log(images);
   function handleSearchSubmit(e) {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-      .then ((res) => res.json())
-      .then ((data) => {
-      console.log(data);
+
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
-      })
+      });
+    setWord("");
   }
-  
+
   return (
     <div>
       <Header title="Images Gallery" />
